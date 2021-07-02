8a59367348d1628a214474bc68da45441d534f0c
ntwari <ntwariegide2@gmail.com>
2021-07-02 21:43:21 +0200

    added orders controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { OrdersController } from './orders/orders.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, OrdersController],
   providers: [AppService],
 })
 export class AppModule {}
