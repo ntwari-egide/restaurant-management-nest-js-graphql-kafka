051bd7199d66217828b464164fdbea4d569cd1e1
ntwari <ntwariegide2@gmail.com>
2021-07-02 23:10:39 +0200

    added customers dto

---
+++ src/customers/model/dto/customer.ts
@@ -0,0 +1,25 @@
+export class Customer{
+
+    customerId : string
+
+    password: string
+
+    firstName : string
+
+    secondName : string
+
+    email : string
+
+    gender : string
+
+    address : string
+
+    postcode : string
+
+    town : string
+
+    state: string
+
+    status : Boolean
+
+}
