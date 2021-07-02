6143443555deb5c6e90b9f176bbd7dcb69e87599
ntwari <ntwariegide2@gmail.com>
2021-07-03 01:55:49 +0200

    added order schema

---
+++ src/orders/model/order.model.ts
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+export const OrderSchema = mongoose.schema({
+
+    orderId: String,
+
+    orderDate: Date,
+
+    customerId: String,
+
+    status: String,
+
+    menuName: String,
+
+    description: String,
+
+    price: Number,
+
+    shipmentStatus: String,
+
+    orderQuantity: Number,
+
+    totalPrice: Number
+})
