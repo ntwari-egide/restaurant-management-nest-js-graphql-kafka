9645a14af6778f72488b372b26d88792a5fb1376
ntwari <ntwariegide2@gmail.com>
2021-07-02 22:50:43 +0200

    order dto

---
+++ src/orders/model/dto/order.interface.ts
@@ -0,0 +1,19 @@
+export interface IOrder{
+
+    orderId: string
+
+    orderDate: Date
+
+    customerId: string
+
+    status: string
+
+    menuName: string
+
+    description: string
+
+    price: Number
+
+    shipmentStatus: string
+    
+}
--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Post, Req } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { IOrder } from './model/dto/order.interface';
+import { OrdersService } from './orders.service';
 
-@Controller('orders')
-export class OrdersController {}
+@Controller('/api/v1/orders')
+export class OrdersController {
+
+    constructor(private readonly ordersService: OrdersService){}
+
+    @Get('/')
+    getallorders(){
+        return this.ordersService.getAllOrders()
+    }
+
+    @Post('/')
+    createPost(@Body() newOrder:IOrder){
+
+        return this.ordersService.createNewOrder(newOrder)
+    
+    }
+
+}
