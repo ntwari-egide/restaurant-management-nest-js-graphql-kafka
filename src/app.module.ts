92a867c723a35448748902fa54a95b495d64f9b9
ntwari <ntwariegide2@gmail.com>
2021-07-02 21:45:10 +0200

    added orders module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OrdersController } from './orders/orders.controller';
 import { OrdersService } from './orders/orders.service';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [],
+  imports: [OrdersModule],
   controllers: [AppController, OrdersController],
   providers: [AppService, OrdersService],
 })
