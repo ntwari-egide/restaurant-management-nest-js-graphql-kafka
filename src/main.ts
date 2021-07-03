274091f53e74461a49b4257ba6291fc848a15e81
ntwari <ntwariegide2@gmail.com>
2021-07-03 18:34:31 +0200

    added swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('restaurant orers management api')
+    .setDescription('restaurant orders management api where one part is Gql and another is RESTFUL apis')
+    .setVersion('1.0')
+    .addTag('orders')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1', app, document);
+
+  await app.listen(3000);
+  
   await app.listen(3000);
 }
 bootstrap();
